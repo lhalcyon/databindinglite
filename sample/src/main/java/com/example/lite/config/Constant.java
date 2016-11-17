5106ce52d67eb5e690a7e6d0506ddcee9eff4bc5
lhalcyon <lh_halcyondays@163.com>
2016-11-17 16:53:25 +0800

    few change

---
+++ sample/src/main/java/com/example/lite/config/Constant.java
@@ -0,0 +1,18 @@
+package com.example.lite.config;
+
+/**
+ * ©2016-2017 kmhealthcloud.All Rights Reserved <p/>
+ * Created by: L  <br/>
+ * Description:
+ */
+
+public interface Constant {
+
+    String IMAGE_ERROR = null;
+    String IMAGE_1 = "http://img14.360buyimg.com/cms/s390x130_jfs/t3583/348/1585950854/29159/1e8180c4/582ad14aN76d0aad4.jpg";
+    String IMAGE_2 = "http://img11.360buyimg.com/cms/s390x130_jfs/t3397/37/1503046916/21781/c9a9592d/582ad0abN628d6acf.jpg";
+
+    String[] IMAGE_RES = new String[]{
+            IMAGE_ERROR, IMAGE_1, IMAGE_2
+    };
+}
