87dd0eb9ed21a322dcc7c7a78ecced93165663c3
halcyon <lh_halcyondays@163.com>
2016-11-22 17:03:39 +0800

    todo picasso load

--- sample/src/main/java/com/example/lite/ui/image/PicassoImageFragment.java
+++ sample/src/main/java/com/example/lite/ui/image/PicassoImageFragment.java
@@ -1,11 +1,15 @@
 package com.example.lite.ui.image;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.lite.R;
+import com.example.lite.databinding.FragmentPicassoBinding;
 
 /**
  * ©2016-2017 kmhealthcloud.All Rights Reserved <p/>
@@ -18,7 +22,9 @@ public class PicassoImageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentPicassoBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_picasso,container,false);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        return binding.getRoot();
     }
 }
