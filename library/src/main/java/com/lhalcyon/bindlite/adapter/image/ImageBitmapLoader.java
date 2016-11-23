0b961a2055e5474ea491caa361de9953303a08fd
halcyon <lh_halcyondays@163.com>
2016-11-23 17:44:07 +0800

    todo imageloader load

--- library/src/main/java/com/lhalcyon/bindlite/adapter/image/ImageBitmapLoader.java
+++ library/src/main/java/com/lhalcyon/bindlite/adapter/image/ImageBitmapLoader.java
@@ -1,9 +1,14 @@
 package com.lhalcyon.bindlite.adapter.image;
 
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.widget.ImageView;
 
 import com.lhalcyon.bindlite.adapter.image.config.BitmapShape;
 import com.lhalcyon.bindlite.adapter.image.config.ScaleType;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.DisplayImageOptions.Builder;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by Administrator on 2016/11/23.
@@ -13,6 +18,12 @@ public class ImageBitmapLoader implements BitmapLoader {
 
     @Override
     public void load(ImageView target, String url, int placeHolder, int error, BitmapShape shape, ScaleType scaleType) {
-
+        Builder builder = new Builder()
+                .showImageForEmptyUri(placeHolder)
+                .showImageOnFail(error)
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .bitmapConfig(Config.RGB_565);
+        ImageLoader.getInstance().displayImage(url,target,);
     }
 }
