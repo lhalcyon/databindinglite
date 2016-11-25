90b6c15aa9a86de3a909ef972540bd6743cd165d
halcyon <lh_halcyondays@163.com>
2016-11-25 15:35:35 +0800

    commit after publish 2 bintray

--- library/src/main/java/com/lhalcyon/bindlite/adapter/image/config/BitmapShape.java
+++ library/src/main/java/com/lhalcyon/bindlite/adapter/image/config/BitmapShape.java
@@ -1,10 +1,5 @@
 package com.lhalcyon.bindlite.adapter.image.config;
 
-/**
- * ©2016-2017 kmhealthcloud.All Rights Reserved <p/>
- * Created by: L  <br/>
- * Description:
- */
 
 public enum BitmapShape {
     SQUARE(0),CIRCLE(1);
--- library/src/main/java/com/lhalcyon/bindlite/adapter/image/config/ScaleType.java
+++ library/src/main/java/com/lhalcyon/bindlite/adapter/image/config/ScaleType.java
@@ -1,10 +1,5 @@
 package com.lhalcyon.bindlite.adapter.image.config;
 
-/**
- * ©2016-2017 kmhealthcloud.All Rights Reserved <p/>
- * Created by: L  <br/>
- * Description:
- */
 
 public enum ScaleType {
     FitCenter(0),CenterCrop(1);
