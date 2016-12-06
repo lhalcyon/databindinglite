2ae450e244b74f3f963962615126a3fe7325a39d
lhalcyon <lh_halcyondays@163.com>
2016-12-06 18:10:07 +0800

    todo separated sample

---
+++ sample-glide/src/main/java/com/example/sample_glide/event/ChangeEvent.java
@@ -0,0 +1,29 @@
+package com.example.sample_glide.event;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.lite.config.Constant;
+import com.example.lite.model.User;
+
+/**
+ * ©2016-2017 kmhealthcloud.All Rights Reserved <p/>
+ * Created by: L  <br/>
+ * Description:
+ */
+
+public class ChangeEvent {
+
+    private User mUser;
+    private int mIndex;
+    public ChangeEvent(User user) {
+        mUser = user;
+    }
+
+    public void onChange(View v){
+        Button btn = (Button) v;
+        Log.e("onChange",btn.getText().toString());
+        mUser.other(++mIndex % Constant.IMAGE_NAME.length);
+    }
+}
